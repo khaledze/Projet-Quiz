3e510c0c796725e31a454b5c8ec67314e8a63a1e
mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-02-09 12:21:40 +0100

    test

--- nvx.js
+++ nvx.js
@@ -2,72 +2,104 @@ document.getElementById("btn1").addEventListener("click", function() {
     var linkList = document.getElementById("linkList");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn2").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn2").style.marginTop = "180px";
     }
 });
 document.getElementById("btn2").addEventListener("click", function() {
     var linkList = document.getElementById("linkList1");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn3").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn3").style.marginTop = "180px";
     }
 });
 document.getElementById("btn3").addEventListener("click", function() {
     var linkList = document.getElementById("linkList2");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn4").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn4").style.marginTop = "180px";
     }
 });
 document.getElementById("btn4").addEventListener("click", function() {
     var linkList = document.getElementById("linkList3");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+       
     }
 });
 document.getElementById("btn5").addEventListener("click", function() {
     var linkList = document.getElementById("linkList4");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn6").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn6").style.marginTop = "180px";
     }
 });
 document.getElementById("btn6").addEventListener("click", function() {
     var linkList = document.getElementById("linkList5");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn7").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn7").style.marginTop = "180px";
     }
 });
 document.getElementById("btn7").addEventListener("click", function() {
     var linkList = document.getElementById("linkList6");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        document.getElementById("btn8").style.marginTop = "0";
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        document.getElementById("btn8").style.marginTop = "180px";
     }
 });
 document.getElementById("btn8").addEventListener("click", function() {
     var linkList = document.getElementById("linkList7");
     if (linkList.style.display === "block") {
         linkList.style.display = "none";
+        linkList.style.paddingTop = "60px";
+        linkList.style.animation = "slideDown 0.5s ease";
+        
     } else {
         linkList.style.display = "block";
-        linkList.scrollIntoView({behavior: "smooth"});
+        linkList.style.paddingTop = "50px";
+        
     }
 });
 
