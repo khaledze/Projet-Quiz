8e7464a03e845d2359c87072ed672319bb804b21
mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-02-09 15:46:33 +0100

    test

--- Acceuil2.js
+++ Acceuil2.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const loginButton = document.getElementById('login');
 const signUpButton = document.getElementById('signUp');
+const connectButton = document.getElementById('connect');
+const registerButton = document.getElementById('register');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add('panel-active');
@@ -9,3 +11,6 @@ signUpButton.addEventListener('click', () => {
 loginButton.addEventListener('click', () => {
 	container.classList.remove('panel-active');
 })
+connectButton.addEventListener("click", function() {
+	window.location.replace("http://127.0.0.1:5500/Projet-Quiz/html/Jeux.html");
+  });
