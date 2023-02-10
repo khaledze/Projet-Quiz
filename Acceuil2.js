70de0cf0e1aa733fc3b558942c6215e054479182
khaledze <k.aliahmed@ecole-ipssi.net>
2023-02-10 09:54:52 +0100

    moi

--- Acceuil2.js
+++ Acceuil2.js
@@ -20,3 +20,15 @@ loginButton.addEventListener('click', () => {
   document.getElementById("connect").addEventListener("click", function(){
     window.location.href = "/Projet-Quiz/html/Jeux.html";
     });
+const toggleSwitch = document.querySelector("#toggle-switch");
+const user = document.querySelector("#user");
+const quizzer = document.querySelector("#quizzer");
+  toggleSwitch.addEventListener("change", function() {
+    if (toggleSwitch.checked) {
+       user.classList.remove("animated");
+        quizzer.classList.add("animated");
+    } else {
+       user.classList.add("animated");
+       quizzer.classList.remove("animated");
+    }
+  });
