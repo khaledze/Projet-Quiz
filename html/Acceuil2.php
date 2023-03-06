664f082c3f6c2cb3386e894f486124e806d4328b
mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-03-06 11:56:24 +0100

    test

--- html/Acceuil2.php
+++ html/Acceuil2.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "quizz";
+    $dbname = "quiz";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
--- question.php
+++ question.php
@@ -7,73 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        class capitale {
-            public $question;
-            public $options;
-            public $answer;
-                
-            public function __construct($question,$options,$answer) {
-                $this->question = $question;
-                $this->options = $options;
-                $this->answer = $answer;
-            }
-        }
-        $question = array(
-            new capitale(
-                "Quelle est la capitale administrative du Maroc ?",
-                array("Casablanca", "Rabat", "Essaouira", "Marrakech"),
-                "Rabat"),
-            new capitale(
-                "Quelle est la capitale de l’Italie ?",
-                array("Rome", "Milan", "Naples", "Florence"),
-                "Rome"),
-            new capitale(
-                "Quelle est la capitale de Cuba ?",
-                array("Saint-Domingue", "La Havane", "Kingston", "Santiago de Cuba"),
-                "La Havane"),
-            new capitale(
-                "Quelle est la capitale du Nigeria ?",
-                array("Abuja", "Kano", "Accra", "Libreville"),
-                "Abuja"),
-            new capitale(
-                "Quelle est la capitale du Yémen ?",
-                array("Mascate", "Abou Dabi", "Sanaa", "Sakaka"),
-                "Sanaa"),
-            new capitale(
-                "Quelle est la capitale d’Haïti ?",
-                array("Las Terrenas", "Port-au-Prince", "Nassau", "Veracruz"),
-                "Port-au-Prince"),
-            new capitale(
-                "Quelle est la capitale de l’Équateur ?",
-                array("Medellin", "San José", "La Paz", "Quito"),
-                "Quito"),
-            new capitale(
-                "Quelle est la capitale de l’Ukraine ?",
-                array("Mariaoupol", "Odessa", "Varsovie", "Kiev"),
-                "Kiev"),
-            new capitale(
-                "Quelle est la capitale de l’Irlande ?",
-                array("Cork", "Belfast", "Dublin", "Glasgow"),
-                "Dublin"),
-            new capitale(
-                "Quelle est la capitale du Ghana ?",
-                array("Tombouctou", "Accra", "Bangui", "Djibouti"),
-                "Accra"),
-        );  
-                $questionCount = count($question);
-                $playerScore = 0;
-                for ($i = 0; $i < $questionCount; $i++) {
-                    $currentQuestion = $question[$i];
-                    $userAnswer = $_POST["question ".$i];
-                    if ($userAnswer == $currentQuestion["answer"]) {
-                        $playerScore++;
-                    }
-                }
-                echo "votre score est de $playerScore";
-    ?>
-
+<?php
+ $host ="localhost";
+$user ="root";
+$password ="";
+$database ="quiz";
+$connect = mysqli_connect($host,$user,$password,$database);
+?>
 </body>
 </html>
-
-
