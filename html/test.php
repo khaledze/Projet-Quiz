dc024199cd1bac37e329c648ac18d0458a46d935
khaledze <k.aliahmed@ecole-ipssi.net>
2023-03-06 11:00:35 +0100

    moi

--- html/test.php
+++ html/test.php
@@ -1,19 +1,13 @@
 <!DOCTYPE html>
+  <!-- Coding by CodingLab | www.codinglabweb.com -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
 </head>
 <body>
-<?php
- $host ="localhost";
-$user ="root";
-$password ="";
-$database ="quiz";
-$connect = mysqli_connect($host,$user,$password,$database);
 
-?>
 </body>
 </html>
