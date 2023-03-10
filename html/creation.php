7764bb6183490cdec918d12a543cf3ef32aa3cbb
mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-03-10 11:24:30 +0100

    test

--- html/creation.php
+++ html/creation.php
@@ -1,111 +1,90 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>Créer un Quiz</title>
-	<link rel="stylesheet" href="/Projet-Quiz/creation.css">
+	<title>Création de quiz</title>
+  <link rel="stylesheet" href="/Projet-Quiz/creation.css">
 </head>
 <body>
 <?php
-// Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "data";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Récupérer les valeurs du formulaire
+  $question = $_POST['question'];
+  $reponse1 = $_POST['reponse1'];
+  $reponse2 = $_POST['reponse2'];
+  $reponse3 = $_POST['reponse3'];
+  
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérification de la connexion
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Si la connexion est établie avec succès, on continue avec le reste du code
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Insertion de la question dans la table 'question'
- 
-
-  $question = mysqli_real_escape_string($conn, $_POST['question']);
-  $sql = "INSERT INTO question (question) VALUES ('$question')";
-
-
-
-  if ($conn->query($sql) === TRUE) {
-    $question_id = $conn->insert_id;
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  if ($_POST['choix1'] == 'reponse1') {
+	$bonneReponse = $reponse1;
+  } elseif ($_POST['choix1'] == 'reponse2') {
+	$bonneReponse = $reponse2;
+  } elseif ($_POST['choix1'] == 'reponse3') {
+	$bonneReponse = $reponse3;
   }
 
-  // Insertion des réponses dans la table 'choix'
-  $answer1 = $_POST['answer1'];
-  $answer2 = $_POST['answer2'];
-  $answer3 = $_POST['answer3'];
-  $correct_answer_num = $_POST['correct_answer'];
-$correct_answer = "";
-switch ($correct_answer_num) {
-  case "1":
-    $correct_answer = mysqli_real_escape_string($conn, $_POST['answer1']);
-    break;
-  case "2":
-    $correct_answer = mysqli_real_escape_string($conn, $_POST['answer2']);
-    break;
-  case "3":
-    $correct_answer = mysqli_real_escape_string($conn, $_POST['answer3']);
-    break;
-  default:
-    // handle invalid input
-}
+  // Se connecter à la base de données
+  $host = "localhost"; 
+  $user = "root";
+  $password = ""; 
+  $dbname = "data"; 
 
+  $conn = mysqli_connect($host, $user, $password, $dbname);
 
-$sql = "INSERT INTO choix (reponse1, reponse2, reponse3, bonneReponse, id_question)
-VALUES ('$answer1', '$answer2', '$answer3', '$correct_answer', LAST_INSERT_ID())";
-if ($conn->query($sql) === TRUE) {
-    // echo "Quiz créé avec succès";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  // Vérifier la connexion
+  if (!$conn) {
+    die("La connexion à la base de données a échoué : " . mysqli_connect_error());
   }
-}
 
-$conn->close();
+  // Insérer la question dans la table question
+  $question = mysqli_real_escape_string($conn, $question);
+  $sql_question = "INSERT INTO question (question) VALUES ('$question')";
+
+  if (mysqli_query($conn, $sql_question)) {
+    // echo "La question a été enregistrée avec succès.";
+  } else {
+    echo "Erreur : " . $sql_question . "<br>" . mysqli_error($conn);
+  }
+
+  // Récupérer l'ID de la question insérée
+  $question_id = mysqli_insert_id($conn);
+
+  // Insérer les réponses dans la table choix
+  $sql_choix = "INSERT INTO choix (reponse1, bonneReponse, reponse2, reponse3, id_question) 
+              VALUES ('$reponse1', '$bonneReponse', '$reponse2', '$reponse3', '$question_id')";
+
+  if (mysqli_query($conn, $sql_choix)) {
+    // echo "Les réponses ont été enregistrées avec succès.";
+  } else {
+    echo "Erreur : " . $sql_choix . "<br>" . mysqli_error($conn);
+  }
+
+  // Fermer la connexion à la base de données
+  mysqli_close($conn);
+}
 ?>
 
+	<form method="post" action="">
+		<label for="question">Question 1 :</label><br>
+		<input type="text" id="question" name="question" required><br>
 
+		<label for="reponse1">Réponse 1 :</label><br>
+		<input type="text" id="reponse1" name="reponse1" required><br>
 
+		<label for="reponse2">Réponse 2 :</label><br>
+		<input type="text" id="reponse2" name="reponse2" required><br>
 
-<!-- formulaire pour créer un quiz -->
-<form method="POST" action="" id="formulaire">
-  <label for="theme">Thème:</label><br>
-  <input type="text" id="theme" name="theme"><br><br>
-  
-  <div id="questions-container">
-    <div class="question">
-      <label for="question1">Question 1:</label><br>
-      <input type="text" id="question1" name="question[]"><br>
+		<label for="reponse3">Réponse 3 :</label><br>
+		<input type="text" id="reponse3" name="reponse3" required><br>
 
-      <label for="answer1_1">Réponse 1:</label><br>
-      <input type="text" id="answer1_1" name="answer1[]"><br>
+		<label for="choix1">Choix de la bonne réponse :</label><br>
+		<select id="choix1" name="choix1" required>
+			<option value="">--Choisir la bonne réponse--</option>
+			<option value="reponse1">Réponse 1</option>
+			<option value="reponse2">Réponse 2</option>
+			<option value="reponse3">Réponse 3</option>
+		</select><br><br>
 
-      <label for="answer2_1">Réponse 2:</label><br>
-      <input type="text" id="answer2_1" name="answer2[]"><br>
-
-      <label for="answer3_1">Réponse 3:</label><br>
-      <input type="text" id="answer3_1" name="answer3[]"><br>
-
-      <label for="correct_answer_1">Bonne réponse:</label><br>
-      <select id="correct_answer_1" name="correct_answer[]">
-        <option value="1">Réponse 1</option>
-        <option value="2">Réponse 2</option>
-        <option value="3">Réponse 3</option>
-      </select><br><br>
-    </div>
-  </div>
-
-  <!-- <input type="button" value="Ajouter une question" onclick="ajouterQuestion()"><br><br> -->
-
-  <input type="submit" value="Créer le quiz">
-</form>
-
-
+		<input type="submit" value="Enregistrer">
+	</form>
 </body>
-<script src="/Projet-Quiz/creation.js"></script>
 </html>
