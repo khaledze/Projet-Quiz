e659b30d147885ca2d98f4621b89bb01b36ca70e
lenzees <tcely2003@gmail.com>
2023-03-10 09:56:55 +0100

    test

--- question.php
+++ question.php
@@ -7,47 +7,47 @@
 	<?php
 	session_start(); // Démarrage de la session
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quizz";
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "quizz";
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
+	$conn = mysqli_connect($host, $username, $password, $dbname);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
 
-// Récupérer le thème choisi
-if (isset($_GET['theme'])) {
-	$theme = htmlspecialchars($_GET['theme']);
-} else {
-	echo "Aucun thème sélectionné.";
-	exit;
-}
+	// Récupérer le thème choisi
+	if (isset($_GET['theme'])) {
+		$theme = htmlspecialchars($_GET['theme']);
+	} else {
+		echo "Aucun thème sélectionné.";
+		exit;
+	}
 
-// Récupération des questions
-if ($theme != "") {
-	$query = "SELECT q.id_question, q.Question, c.reponse1, c.reponse2, c.reponse3, c.bonneReponse 
+	// Récupération des questions
+	if ($theme != "") {
+		$query = "SELECT q.id_question, q.Question, c.reponse1, c.reponse2, c.reponse3, c.bonneReponse 
 			  FROM question q 
 			  INNER JOIN choix c ON q.id_question = c.id_question 
 			  WHERE c.theme = '$theme'";
-} else {
-	$query = "SELECT q.id_question, q.Question, c.reponse1, c.reponse2, c.reponse3, c.bonneReponse 
+	} else {
+		$query = "SELECT q.id_question, q.Question, c.reponse1, c.reponse2, c.reponse3, c.bonneReponse 
 			  FROM question q 
 			  INNER JOIN choix c ON q.id_question = c.id_question";
-}
-$result = mysqli_query($conn, $query);
-if (!$result) {
-	die('Erreur de récupération des questions : ' . mysqli_error($conn));
-}
+	}
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		die('Erreur de récupération des questions : ' . mysqli_error($conn));
+	}
 
-// Affichage des questions et des réponses
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<p>" . $row['Question'] . "</p>";
-    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse1'] . "'> " . $row['reponse1'] . "</label><br>";
-    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse2'] . "'> " . $row['reponse2'] . "</label><br>";
-    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse3'] . "'> " . $row['reponse3'] . "</label><br>";   
-}
+	// Affichage des questions et des réponses
+	while ($row = mysqli_fetch_assoc($result)) {
+	    echo "<p>" . $row['Question'] . "</p>";
+	    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse1'] . "'> " . $row['reponse1'] . "</label><br>";
+	    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse2'] . "'> " . $row['reponse2'] . "</label><br>";
+	    echo "<label><input type='radio' name='reponse_" . $row['id_question'] . "' value='" . $row['reponse3'] . "'> " . $row['reponse3'] . "</label><br>";   
+	}
 
 // Vérification des réponses et calcul du score
 if (isset($_POST['submit'])) {
