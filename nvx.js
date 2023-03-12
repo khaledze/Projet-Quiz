4deb349d79c0f23e9fd749c14c661e57a927fb45
khaledze <k.aliahmed@ecole-ipssi.net>
2023-03-12 12:57:28 +0100

    moi

--- nvx.js
+++ nvx.js
@@ -1,24 +1,24 @@
-document.getElementById("quit").addEventListener("click", function()
-{ 
-     window.location.href = "/Projet-Quiz/html/Jeux.html";
-});
+// document.getElementById("quit").addEventListener("click", function()
+// { 
+//      window.location.href = "/Projet-Quiz/html/Jeux.html";
+// });
 
-document.getElementById("userType").addEventListener("change", function() {
-     var selectedValue = this.value;
-     if (selectedValue === "utilisateur") {
-       window.location.href = "jouer.html";
-     } else if (selectedValue === "quizzer") {
-       window.location.href = "creer.html";
-     }
-   });
-const AnimText = document.querySelector("h1");
+// document.getElementById("userType").addEventListener("change", function() {
+//      var selectedValue = this.value;
+//      if (selectedValue === "utilisateur") {
+//        window.location.href = "jouer.html";
+//      } else if (selectedValue === "quizzer") {
+//        window.location.href = "creer.html";
+//      }
+//    });
+// const AnimText = document.querySelector("h1");
 
-const typewriter = new Typewriter(AnimText, {
-     loop: true ,
-     deleteSpeed:20
-})
-.changeDelay(50)
-.typeString("Bienvenue dans notre Jeu Quizzeo")
-.pauseFor(300)
-.start()
+// const typewriter = new Typewriter(AnimText, {
+//      loop: true ,
+//      deleteSpeed:20
+// })
+// .changeDelay(50)
+// .typeString("Bienvenue dans notre Jeu Quizzeo")
+// .pauseFor(300)
+// .start()
 
