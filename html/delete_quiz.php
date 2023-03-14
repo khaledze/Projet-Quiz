d8cc875e4ad73671b015e87567a546137d423cc2
mass1n1ssa <m.amanzougaghene@ecole-ipssi.net>
2023-03-14 20:55:59 +0100

    test

--- html/delete_quiz.php
+++ html/delete_quiz.php
@@ -18,20 +18,23 @@ if (isset($_GET['theme'])) {
     }
 
     // Prepare the DELETE query
-    $sql = "DELETE FROM quizz WHERE titre='$theme'";
+    $sql_titre = "DELETE FROM quizz WHERE titre='$theme'";
+    $sql_question = "DELETE FROM question WHERE id_question IN (SELECT id_question FROM choix WHERE theme='$theme')";
+    $sql_choix = "DELETE FROM choix WHERE theme='$theme'";
 
     // Execute the query
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql_titre) && mysqli_query($conn, $sql_question) && mysqli_query($conn, $sql_choix)) {
         header("Location: /Projet-Quiz/html/Jeux3.php");
         exit();
     } else {
-        // echo "Erreur lors de la suppression du quiz: " . mysqli_error($conn);
+        echo "Erreur lors de la suppression du quiz: " . mysqli_error($conn);
     }
 
     // Close the database connection
     mysqli_close($conn);
 } else {
     // Redirect the user back to the page with the quiz list
-    header("Location: /Projet-Quiz/html/Jeux3.php");
-    exit();
+    // header("Location: /Projet-Quiz/Jeux3.php");
+    // exit();
 }
+
