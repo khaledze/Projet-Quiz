6a628af252a2f9c8d8410fdf61994752929edf73
lenzees <tcely2003@gmail.com>
2023-03-14 12:00:19 +0100

    test

--- html/Jeux.php
+++ html/Jeux.php
@@ -3,7 +3,7 @@
     $host = "localhost"; 
     $user = "root";
     $password = ""; 
-    $dbname = "data"; 
+    $dbname = "quizz"; 
 
     $conn = mysqli_connect($host, $user, $password, $dbname);
 
